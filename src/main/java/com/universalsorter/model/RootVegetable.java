4987437b1e25742e1c2f0dcc3cd4e06cae8c5057
EvgenSharapov <dooplert@yandex.ru>
2025-01-14 14:52:05 +0300

    Добавлены кастомные классы.Добавлено меню.

--- src/main/java/com/universalsorter/model/RootVegetable.java
+++ src/main/java/com/universalsorter/model/RootVegetable.java
@@ -1,4 +1,64 @@
 package com.universalsorter.model;
+import lombok.Getter;
+
+
 
 public class RootVegetable {
+
+    private final String type;
+    private final Double weight;
+    private final Color color;
+
+    public RootVegetable(String type, Double weight, Color color) {
+        this.type = type;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public static RootVegetable.Builder builder() {
+        return new RootVegetable.Builder();
+    }
+
+    public static class Builder {
+        private String type;
+        private Double weight;
+        private Color color;
+
+
+        public RootVegetable.Builder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public RootVegetable.Builder weight(Double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public RootVegetable.Builder color(Color color) {
+            this.color = color;
+            return this;
+        }
+
+        public RootVegetable build() {
+            if (type == null || weight <=0 || color ==null) {
+                throw new IllegalStateException("Введены некорректные данные");
+            }
+            return new RootVegetable(type, weight, color);
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+
 }
--- src/main/java/com/universalsorter/utils/InputUtils.java
+++ src/main/java/com/universalsorter/utils/InputUtils.java
@@ -1,4 +1,124 @@
 package com.universalsorter.utils;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class InputUtils {
+    private Scanner scanner;
+    private int arraySize;
+
+    public InputUtils() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void execute(){
+        int selection = fillingTheArray();
+        menuSelection(selection);
+        arraySize=takeArraySize();
+        //создание массива
+
+        getArrayElement();
+
+
+    }
+
+    private int fillingTheArray(){
+        int selection=0;
+        while (true) {
+            String temp = getInput("Выберите вариант заполнения массива: \n1 - Из файла\n2 - Случайно\n3 - Вручную\n0 - Выход");
+            if (temp.matches("^[0-3]$")) {
+                selection=Integer.parseInt(temp);
+                break;
+            } else {
+
+                displayError("Неправильный ввод. Пожалуйста, введите 0, 1, 2 или 3.");
+            }
+        }
+        return selection;
+    }
+
+    private void menuSelection(int i){
+        int select=0;
+        Random random=new Random();
+        int rand=random.nextInt(3)+1;
+        switch (i){
+            case 1:break;//loadFromFile();Загрузка из файла
+            case 2:loadCustomClass(rand);break;
+            case 3:manualLoad();break;
+            default:displayError("Неизвестная операция.");
+        }
+
+    }
+
+    private int takeArraySize() {
+        int size=0;
+        while (true) {
+            String temp = getInput("Выберите длину заполняемого массива :");
+
+            if (temp.matches("^[1-9][0-9]*$")) {
+                size=Integer.parseInt(temp);
+                break;
+            } else {
+
+                displayError("Неправильный ввод. Пожалуйста, введите целое неотрацательное число(первая цифра не должна начинаться с 0).");
+            }
+        }
+        return size;
+    }
+
+    private void getArrayElement(){
+        while (true) {
+            System.out.print("Выберите элемент массива: \n");
+            Scanner scanner = new Scanner(System.in);
+            String operation = scanner.next();
+            if (operation.matches("^(0|[1-9][0-9]*)$")){
+                int element = Integer.parseInt(operation);//номер элемента массива
+               //метод для получения элемента массива ;
+                }
+                else{
+                    displayError("Неизвестная операция");
+                }
+        }
+    }
+
+    private String getInput(String message){
+        System.out.println(message);
+        return scanner.next();
+    }
+
+
+    private void manualLoad(){
+        int selection=0;
+        while (true) {
+            String temp = getInput("Выберите, какими данными заполнить массив: \n1 - Book\n2 - Car\n3 - RootVegetable\n0 - Назад");
+            if (temp.matches("^[0-3]$")) {
+                selection=Integer.parseInt(temp);
+                if(selection==0){execute();}
+                break;
+            } else {
+
+                displayError("Неправильный ввод. Пожалуйста, введите 0, 1, 2 или 3.");
+            }
+        }
+        loadCustomClass(selection);
+    }
+
+    private void loadCustomClass(int choice){
+        switch (choice){
+            case 1 :
+                System.out.println("Выбран класс Book");break;//loadBookClass();
+            case 2:
+                System.out.println("Выбран класс Car");break; //loadCarClass();
+            case 3:
+                System.out.println("Выбран класс RootVegetable");break;//loadRootVegetableClass();
+            default:
+                displayError("Неизвестная операция.");
+        }
+    }
+    private void displayError(String error) {
+        System.out.println("Ошибка : "+error);
+    }
+
 }
+
+
