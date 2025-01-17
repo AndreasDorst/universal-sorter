73bd2e6da412ade3d38387fb8bb97e05fa10a7a6
Aleksandr Bogin <bogin.a.v@gmail.com>
2025-01-17 13:02:45 +0300

    US-15.  реализован интерфейс Comparator<Book> в классе Book, класс Book задокументирован, исправлен Comparable<Book>

--- src/main/java/com/universalsorter/model/Book.java
+++ src/main/java/com/universalsorter/model/Book.java
@@ -1,14 +1,42 @@
 package com.universalsorter.model;
+import java.util.Comparator;
 import java.util.Locale;
+import java.util.Objects;
 
-public class Book implements Storable,Comparable {
 
+/**
+ * Class book with properties <b>author</b> and <b>title</b> and <b>numberOfPages</b>  .
+ * {@code @autor} Аднрей, Евгений, Александр
+ * @version 1.1
+ */
+public class Book implements Storable,Comparable<Book>, Comparator<Book> {
 
+    /**
+     * Author of the book
+     * --GETTER--
+     *  Return the author of the book.
+     * */
     private final String author;
+    /**
+     * Title of the book
+     * --GETTER--
+     *  Return the title of the book.
+     * */
     private final String title;
+    /**
+     * Number of pages of the book
+     * --GETTER--
+     *  Return the number of pages of the book.
+     * */
     private final Integer numberOfPages;
 
-
+    /**
+     * Конструктор - создание нового объекта
+     * @param author - автор
+     * @param title - название
+     * @param numberOfPages - кол-во страниц
+     * @see Book#Book(String , String , Integer )
+     */
     private Book(String author, String title, Integer numberOfPages) {
         this.author = author;
         this.title = title;
@@ -16,21 +44,29 @@ public class Book implements Storable,Comparable {
     }
 
 
-
+    /**
+     * Метод для создания объекта Book {@link Book}
+     * @return возвращает название производителя
+     */
     public static Book.Builder builder() {
         return new Book.Builder();
     }
 
+    /**
+     *  Переопределенный метод интерфейса Comparable {@link Comparable}
+     * @param book объект для сравнения
+     * @return отрицательное целое число, ноль или положительное целое число, поскольку этот объект меньше, равен или больше указанного объекта
+     */
     @Override
-    public int compareTo(Object o) {
-        if (o == null) {
+    public int compareTo(Book book) {
+        if (book == null) {
             throw new NullPointerException("Сравниваемый объект не может быть null");
         }
-        if (!(o instanceof Book)) {
-            throw new ClassCastException("Объект должен быть типа Book");
-        }
-
-        Book book = (Book) o;
+//        if (!(o instanceof Book)) {
+//            throw new ClassCastException("Объект должен быть типа Book");
+//        }
+//
+//        Book book = (Book) o;
 
         // Сравнение по автору
         int authorComparison = this.author.compareTo(book.author);
@@ -48,6 +84,27 @@ public class Book implements Storable,Comparable {
         return this.numberOfPages.compareTo(book.numberOfPages);
     }
 
+
+    /**
+     *  Переопределенный метод интерфейса Comparator {@link Comparator}
+     * @param book1 первая книга для сравнения.
+     * @param book2 вторая книга для сравнения.
+     * @return отрицательное целое число, ноль или положительное целое число, поскольку первый аргумент меньше, равен или больше второго.
+     */
+    @Override
+    public int compare(Book book1, Book book2) {
+        int authorComparison = book1.getAuthor().compareTo(book2.getAuthor());
+        if (authorComparison != 0) {
+            return authorComparison;
+        }
+
+        int titleComparison = book1.getTitle().compareTo(book2.getTitle());
+        if (titleComparison != 0) {
+            return titleComparison;
+        }
+
+        return book1.getNumberOfPages().compareTo(book2.getNumberOfPages());
+    }
 
 
     public static class Builder {
@@ -82,20 +139,34 @@ public class Book implements Storable,Comparable {
             return this;
         }
 
+
         public Book build() {
             validate();
             return new Book(author, title, numberOfPages);
         }
     }
 
+
+    /**
+     * Функция получения значения поля {@link Book#author}
+     * @return возвращает имя автора книги.
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Функция получения значения поля {@link Book#title}
+     * @return возвращает название книги.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Функция получения значения поля {@link Book#numberOfPages}
+     * @return возвращает кол-во страниц в книге
+     */
     public Integer getNumberOfPages() {
         return numberOfPages;
     }
@@ -119,6 +190,34 @@ public class Book implements Storable,Comparable {
                 .build();
     }
 
+
+
+    /**
+     * Указывает, равен ли какой-либо другой объект этому компаратору. Этот метод должен подчиняться общему договору {@link Object#equals(Object)}.
+     * @param o объект для сравнения.
+     * @return {@code true} только если указанный объект также {@link Book} и имеет одинаковые поля.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) && Objects.equals(title, book.title) && Objects.equals(numberOfPages, book.numberOfPages);
+    }
+
+    /**
+     * Переопределенный метод класса {@link Object}, который генерирует и возвращает хэш-кода для объекта {@link Book}. Этот метод должен подчиняться общему договору {@link Object#hashCode()}.
+     * @return возвращает значение хэш-кода для этого объекта класса {@link Book}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, title, numberOfPages);
+    }
+
+    /**
+     * Переопределенный метод класса {@link Object}, возвращает строковое представление объекта. {@link Book}. Этот метод должен подчиняться общему договору {@link Object#hashCode()}.
+     * @return возвращает строковое представление объекта класса {@link Book}
+     */
     @Override
     public String toString() {
         return "Автор: " + author + "\n" +
