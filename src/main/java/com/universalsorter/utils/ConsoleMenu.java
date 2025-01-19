bd56b74fa9b6bbfe67d4dcaecb8400adb1160f7b
Stim1k <stim1k159@gmail.com>
2025-01-19 16:58:22 +0300

    Фикс функционала для заполнения массивов вручную.

--- src/main/java/com/universalsorter/utils/ConsoleMenu.java
+++ src/main/java/com/universalsorter/utils/ConsoleMenu.java
@@ -1,10 +1,9 @@
 package com.universalsorter.utils;
 import java.io.IOException;
+import java.time.Year;
 import java.util.Scanner;
-import com.universalsorter.model.Book;
-import com.universalsorter.model.Car;
-import com.universalsorter.model.RootVegetable;
-import com.universalsorter.model.SupportedTypes;
+
+import com.universalsorter.model.*;
 
 public class ConsoleMenu {
     private ArrayManager arrayManager;
@@ -139,7 +138,7 @@ public class ConsoleMenu {
 
         System.out.println("\n1. Загрузить данные из программы");
         System.out.println("2. Загрузить данные из файла");
-        System.out.println("3. Ввести данные вручную");
+        System.out.println("3. Загрузить данные вручную");
         System.out.println("0. Назад");
         System.out.print("Выберите пункт меню: ");
         String choice = scanner.next();
@@ -163,7 +162,7 @@ public class ConsoleMenu {
             	arrayManager.downloadDataFromFile(dataType);
                 break;
             case 3:
-            	System.out.println("\nФункционал находится в разработке.\n");
+                arrayManager.fillArrayWithChosenData(dataType);
                 break;
             case 0:
                 break;
